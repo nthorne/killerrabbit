8016da1ce6d69da73411aacc1ddcefce9c977b10
nthorne <notrupertthorne@gmail.com>
2013-01-05 22:11:27 +0100

    Added licensing information

--- test/client.py
+++ test/client.py
@@ -1,5 +1,22 @@
 #!/usr/bin/env python
 
+#Copyright (C) 2013 Niklas Thorne.
+
+#This file is part of killerrabbit.
+#
+#killerrabbit is free software: you can redistribute it and/or modify
+#it under the terms of the GNU General Public License as published by
+#the Free Software Foundation, either version 3 of the License, or
+#(at your option) any later version.
+#
+#killerrabbit is distributed in the hope that it will be useful,
+#but WITHOUT ANY WARRANTY; without even the implied warranty of
+#MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#GNU General Public License for more details.
+#
+#You should have received a copy of the GNU General Public License
+#along with killerrabbit.  If not, see <http://www.gnu.org/licenses/>.
+
 """ This application is used to test killerrabbit.
 
 It connects to a server (via killerrabbit), sends some data to it and
