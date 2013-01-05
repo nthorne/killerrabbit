b02b09080311060b0712fd8ea4486a8c9f0c1e03
nthorne <notrupertthorne@gmail.com>
2013-01-05 22:00:28 +0100

    Linted the test client

--- test/client.py
+++ test/client.py
@@ -1,6 +1,11 @@
 #!/usr/bin/env python
 
-#import pickle
+""" This application is used to test killerrabbit.
+
+It connects to a server (via killerrabbit), sends some data to it and
+asserts that the same data is sent back, closes down the connection and
+then starts all over again. """
+
 import socket
 import threading
 import sys
@@ -10,6 +15,10 @@ import logging
 
 # Here's our thread:
 class ConnectionThread(threading.Thread):
+    """ This type establishes a connection with the server, sends some data
+    to it, asserts taht the same data is echoed back, and then terminates
+    the connection. """
+
     def __init__(self, host='localhost', port=9090):
         super(ConnectionThread, self).__init__()
 
@@ -17,46 +26,40 @@ class ConnectionThread(threading.Thread):
         self.__port = port
 
     def run(self):
-        # Connect to the server:
         client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         try:
             client.connect((self.__host, self.__port))
         except:
-            logging.info("Failed to connect to %s:%d" %
-                         (self.__host, self.__port))
+            logging.info("Failed to connect to %s:%d", self.__host,
+                         self.__port)
 
-        # Retrieve and unpickle the list object:
-        #print pickle.loads(client.recv(1024))
-
-        # Send some messages:
-        for x in xrange(42):
+        for tx_byte in xrange(42):
             try:
-                client.send(chr(x))
-                y = client.recv(1)
-                logging.debug("  (%d->, ->%d)" % (x, ord(y)))
+                client.send(chr(tx_byte))
+                rx_byte = client.recv(1)
+                logging.debug("  (%d->, ->%d)", tx_byte, ord(rx_byte))
             except:
                 break
 
-            assert(chr(x) == y)
+            assert(chr(tx_byte) == rx_byte)
 
         logging.debug("Session finished. Closing channel.")
-        # Close the connection
         client.close()
 
 try:
     logging.basicConfig(format='%(asctime)-15s %(message)s')
 
-    logger = logging.getLogger()
-    logger.setLevel(logging.INFO)
+    LOGGER = logging.getLogger()
+    LOGGER.setLevel(logging.INFO)
 
     socket.setdefaulttimeout(5)
 
     logging.info("Starting client..")
 
     while True:
-        c = ConnectionThread()
-        c.start()
-        c.join()
+        CONNECTION = ConnectionThread()
+        CONNECTION.start()
+        CONNECTION.join()
 
         time.sleep(3)
 except KeyboardInterrupt:
